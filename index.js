2d7a03dba705f9d16d71415922cd29e0356952f2
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-03-24 23:29:01 +0530

    ✅

--- index.js
+++ index.js
@@ -34,20 +34,21 @@ const testfunc = async () => {
     }
 }
 
-//Shows all the data
+//Shows all the data                                                                    // Works
 app.get('/api/', async (req, res) => {
     let data = await collection.find({}).toArray();
     res.json(data);
 })
 
-// app.get('/api/', async (req, res) => {                               // Bug Fix it
+// // to show one particular json object at a time according to the ID req
+// app.get('/api/', async (req, res) => {                                     // Bug Fix it
 //     let datain = req.body;
 //     let f = datain._id;
 //     let data = await collection.findOne({ _id: f });
 //     res.json(data);
 // })
 
-// To add a new json file to the MongoDB
+// To add a new json file to the MongoDB                                                // Works
 app.post('/api/', async (req, res) => {
     let data = req.body;
     await collection.insertOne(data);
@@ -55,15 +56,17 @@ app.post('/api/', async (req, res) => {
 })
 
 
-// to edit existing files
-app.put('/api/', async (req, res) => {
-    let internal = req.body;
-    let dat = internal._id;
-    let data = await collection.findOne({ _id: dat });
-    data.name = internal.name;
-    await collection.replaceOne(data);
-    res.send("Replaced Succesfully")
-})
+// // to edit existing files                                            // Bug Fix it
+// app.put('/api/', async (req, res) => {
+//     let internal = req.body;
+//     let dat = internal._id;
+//     let data = await collection.findOne({ _id: dat });
+//     data.name = internal.name;
+//     await collection.replaceOne(data);
+//     res.send("Replaced Succesfully")
+// })
+
+// Pending: To delete a json file, depending on the ID received
 
 
 
